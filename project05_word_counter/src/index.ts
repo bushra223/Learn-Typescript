6ff99ac4a80da187bfdee142d2763ba14e06c13d
Bushra Attique <152737575+bushra223@users.noreply.github.com>
2024-07-17 02:24:38 +0500

    project 5 added word counter

---
+++ project05_word_counter/src/index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+import InputPrompt from "inquirer/lib/prompts/input.js";
+//possible use cases
+//user enter single space, multiple spaces, single tab or multiple tabs
+//our code should handle these cases
+const answer = await inquirer.prompt([
+  {
+    type: "input",
+    name: "text",
+    message: "Please enter the text you want me to count the characters for:",
+  },
+]);
+const textEntered: string = answer.text.trim();
+//\s+ replaces single space or multiple spaces with single space,and g flag means thet reg ex will be tested against all possible occurrences
+let temp1 = textEntered.replace(/\s+/g, " ");
+//now we have the string which has removed all the whitespaces but just one single space between words
+//now we cacn split
+let words: string[] = temp1.split(" ");
+console.log(
+  `You have entered ${words.length} words and ${
+    words.join("").toString().length
+  } characters`
+);
